1b28e0bc7934d5357756eda47bf197f42b75d2c8
Joaquin Dimitroff <joaquindimi.dev@outlook.com>
2023-02-09 13:51:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+//funcion deseleccionar menu//
+function seleccionar(link){
+    var opciones = document.querySelectorAll('#links a');
+    opciones[0].className = ""
+    opciones[1].className = ""
+    opciones[2].className = ""
+    opciones[3].className = ""
+    opciones[4].className = ""
+    link.className = "seleccionado";
+}
+// funcion menu responsive //
+function responsiveMenu(){
+    var x = document.getElementById("nav");
+    if (x.className === ""){
+        x.className = "responsive";
+    } else{
+        x.className = "";
+    }
+}
+
+
+const typed = new Typed('.typed',{
+    strings: [Joaquin, Joaquin]
+})
